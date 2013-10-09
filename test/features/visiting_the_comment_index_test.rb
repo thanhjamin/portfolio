a2d85a16b5892b5c99e92e2757db79b1e8123c5c
Thanh Huynh <thanhjamin@yahoo.com>
2013-10-09 16:22:37 -0700

    visit test done, close #12

--- test/features/visiting_the_comment_index_test.rb
+++ test/features/visiting_the_comment_index_test.rb
@@ -3,12 +3,15 @@ require "test_helper"
 feature "Visiting the Post Index" do
   scenario "with existing posts" do
     # Given existing posts
-
+    @comment = Comment.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
+    visit comments_path
 
     # When I visit /posts
-
+    click_on 'Show'
 
     # Then the existing posts should be loaded
+    page.text.must_include 'Title'
+    page.text.must_include 'Body'
 
   end
 end
