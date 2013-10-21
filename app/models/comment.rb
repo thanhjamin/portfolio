27d6099db382f55261b663df1075304a4666a480
Thanh Huynh <thanhjamin@yahoo.com>
2013-10-21 11:07:56 -0700

    clean up views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   attr_accessible :body, :title, :published
   belongs_to :author, class_name: "User"
+  scope :published, where(published: true)
 
   def publish!
     published = true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@editor = User.create(name: ""
+                      email:""
+                      password: ""
+                      password_confirmation: ""
+                      role: "editor")
